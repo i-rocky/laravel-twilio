f0d2cae0f2f828f78a115157f1f0dbacbd13868e
Rocky <wpkpda@gmail.com>
2019-11-09 10:52:34 +0600

    init

---
+++ src/Providers/LaravelTwilioServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Rocky\LaravelTwilio\Providers;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelTwilioServiceProvider extends ServiceProvider
+{
+    /**
+     * Provides configuration
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Does something
+     */
+    public function register()
+    {
+
+    }
+}
